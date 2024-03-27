e0454ed4014452e17e0059405908051fec591623
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-03-27 13:55:31 +0000

    Mar 27, 2024, 1:55 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -56,7 +56,7 @@ app.signIn.visible = False
 
 connection = sqlite3.connect("database.db")
 print(connection.total_changes)
-connection.execute("CREATE TABLE Tweets (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, content TEXT NOT NULL, date_created INTEGER)")
+connection.execute("CREATE TABLE IF NOT EXISTS Tweets (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, content TEXT NOT NULL, date_created INTEGER)")
 
 
 
