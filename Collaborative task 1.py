ceb7d5f28b0486e305c7914067b04bde911d96a7
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-03-27 13:56:38 +0000

    Mar 27, 2024, 1:56 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -56,8 +56,9 @@ app.signIn.visible = False
 
 connection = sqlite3.connect("database.db")
 print(connection.total_changes)
-connection.execute("CREATE TABLE IF NOT EXISTS Tweets (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, content TEXT NOT NULL, date_created INTEGER)")
-rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
+connection.execute("CREATE TABLE IF NOT EXISTS Tweets (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, icon TEXT content TEXT NOT NULL, date_created INTEGER)")
+rows = connection.execute("SELECT username, content, content FROM Tweets").fetchall()
+print(rows)
 
 
 
