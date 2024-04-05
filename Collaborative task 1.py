0c2c9def1b18b854ac2bcd7a8894aefa39ffc977
SnipaChen <159275360+SnipaChen@users.noreply.github.com>
2024-04-05 13:51:03 +0000

    "2024vPMn" - Apr 5, 2024, 1:51 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -81,12 +81,12 @@ def printTweets():
     full_tweet = Group()  
     for tweet in db:   
         print(tweet)
-        # icon = Image(tweet["icon"], 20,yVal-30)        
-        # username = Label(tweet["username"], 80, yVal-30,font='montserrat',bold=True)
-        # message = Label(tweet["text"],username.right,yVal-10,size=20)
-        # barline=Line(0,message.bottom+30,400,message.bottom+30,opacity=30)
-        # yVal += 67.5
-        # full_tweet.add(icon,username,message, barline)
+        icon = Image(tweet["icon"], 20,yVal-30)        
+        username = Label(tweet["username"], 80, yVal-30,font='montserrat',bold=True)
+        message = Label(tweet["text"],username.right,yVal-10,size=20)
+        barline=Line(0,message.bottom+30,400,message.bottom+30,opacity=30)
+        yVal += 67.5
+        full_tweet.add(icon,username,message, barline)
     app.tweetPage.add(full_tweet)    
 printTweets()
 
