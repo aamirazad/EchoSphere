a928506d158af6ab5ab703126e943540c6e3156a
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-04-10 13:41:36 +0000

    Apr 10, 2024, 1:41 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -165,7 +165,7 @@ def onStep():
             for count, line in enumerate(lines):
                 if not line:
                     print('debug')
-                    app.list_of_lines[count].value = line = ""
+                    app.list_of_lines[count].value = ""
                 else:
                     app.list_of_lines[count].fill = "black"
                     app.list_of_lines[count].value = line
