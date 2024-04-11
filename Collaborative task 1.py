ec806b7428d395a6a615b23c175fb10ee751aba3
SnipaChen <159275360+SnipaChen@users.noreply.github.com>
2024-04-11 13:48:54 +0000

    "2024vPMn" - Apr 11, 2024, 1:48 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -88,7 +88,7 @@ def printTweets():
         message = Label(tweet[3],username.right,yVal-10,size=20)
         barline=Line(0,message.bottom+30,400,message.bottom+30,opacity=30)
         yVal += 67.5
-        full_tweet.add(icon,username,message, barline,arrows)
+        full_tweet.add(icon,username,message, barline)
     app.tweetPage.add(full_tweet)    
 printTweets()
 
