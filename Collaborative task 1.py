c576d1061652584cdcb177e867b8f61458cedb54
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-04-26 13:23:27 +0000

    Apr 26, 2024, 1:23 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -224,11 +224,10 @@ def onKeyPress(key):
         elif key == "enter":
             if app.text.count("\n") < 2:
                 app.text += "\n"
-        valid_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789'
+        valid_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789.!'
         list_of_valid_characters = list(valid_characters)
         if key in list_of_valid_characters:
             app.text += key
-        
         lines = app.text.splitlines()
         if len(lines[-1]) >= 20:
             if app.text.count("\n") < 2:
