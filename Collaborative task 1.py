4a73f793f147c9f09a06ab763434e21c50b37a72
SnipaChen <159275360+SnipaChen@users.noreply.github.com>
2024-04-26 13:22:25 +0000

    Apr 26, 2024, 1:22 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -90,7 +90,7 @@ def printTweets():
             lineYVal = (count * 30) + (yVal+35)
             message.add(Label(line,username.right,lineYVal,size=20))
             message.left = 70
-        
+        Trash=Line
         barline=Line(0,message.bottom+30,400,message.bottom+30,opacity=30)
         yVal = barline.bottom
         app.full_tweet.add(icon,username,message, barline)
