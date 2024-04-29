7af1830fec075de764b492b9701e9effc4939037
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-04-29 13:14:03 +0000

    Apr 29, 2024, 1:14 PM

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -195,7 +195,7 @@ def onMousePress(mouseX,mouseY):
         for tweet in app.full_tweet:
             tweet.centerY -= 150
         app.header.toFront()
-    
+    elif checkClick
 
 # handle keypress
 def onKeyPress(key):
