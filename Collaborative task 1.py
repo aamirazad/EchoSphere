30eb700bc1d61a4ccc5bcc59dda17532cad3c38e
SnipaChen <159275360+SnipaChen@users.noreply.github.com>
2024-04-29 13:15:55 +0000

    mjhmj

--- Collaborative task 1.py
+++ Collaborative task 1.py
@@ -200,7 +200,7 @@ def onMousePress(mouseX,mouseY):
         for tweet in app.full_tweet:
             tweet.centerY -= 150
         app.header.toFront()
-    elif checkClick
+    elif checkClick:
 
 # handle keypress
 def onKeyPress(key):
